402d8b15955b1c61c34a4df154784c16fd239e88
Maks-xex <justmax.xex@mail.ru>
2023-11-02 17:11:03 +0100

    constans

--- src/api/get-data.ts
+++ src/api/get-data.ts
@@ -1,5 +1,5 @@
 import { IData } from "../types";
-import { HOST } from "./constatns";
+import { HOST } from "./constans";
 
 export const getData = async (): Promise<IData[]> => {
   const response = await fetch(`${HOST}/api/slides`);
