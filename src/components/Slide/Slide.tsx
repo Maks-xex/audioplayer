1330d680f2ef18277ff1a5c2a3917079c7340569
Maks-xex <justmax.xex@mail.ru>
2023-11-03 11:17:30 +0100

    split logic

--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -1,85 +1,14 @@
-import React, { useRef, useState } from "react";
-
-import { CSSTransition } from "react-transition-group";
-
-import { IData } from "../../types";
+import React from "react";
 
 import styles from "./slide.module.scss";
-import { Player } from "../audioPlayer/AudioPlayer";
-import { useTransitionControl } from "../../hooks/useTransitionControl";
 
 interface ISlide {
-  content: IData[];
+  children: JSX.Element;
 }
-export const Slide: React.FC<ISlide> = ({ content }) => {
-  const { isTransitioning, startTransition } = useTransitionControl(500);
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [direction, setDirection] = useState<"left" | "right">("left");
-  const transitionRef = useRef(null);
-
-  const handleImageClick = (index: number): void => {
-    if (isTransitioning) {
-      return;
-    }
-    if (index > currentIndex) {
-      setDirection("right");
-    } else {
-      setDirection("left");
-    }
-    startTransition();
-    setCurrentIndex(index);
-  };
-
+export const Slide: React.FC<ISlide> = ({ children }) => {
   return (
     <section className={styles.slider}>
-      <div className={styles.slider_wrapper}>
-        {content.map((data, i) => (
-          <CSSTransition
-            key={data.id}
-            in={currentIndex === i}
-            nodeRef={transitionRef}
-            timeout={300}
-            unmountOnExit
-            classNames={{
-              enterActive: styles[`fade_enter_active_${direction}`],
-              enter: styles[`fade_enter_${direction}`],
-              exitActive: styles[`fade_exit_active_${direction}`],
-              exit: styles[`fade_exit_${direction}`],
-            }}
-          >
-            <div
-              className={
-                i === currentIndex ? styles["is-visible"] : styles.slide
-              }
-              ref={transitionRef}
-            >
-              <img
-                src={data.imageURL}
-                className={styles.cover}
-                alt={data.text}
-              />
-
-              <h3>{data.text}</h3>
-              <Player
-                src={data.audioURL}
-                clickNextHandler={() =>
-                  currentIndex < content.length - 1 &&
-                  handleImageClick(currentIndex + 1)
-                }
-                onEnded={() =>
-                  currentIndex < content.length - 1 &&
-                  handleImageClick(currentIndex + 1)
-                }
-                clickPreviousHandler={() =>
-                  currentIndex !== 0 && handleImageClick(currentIndex - 1)
-                }
-                canClickNext={currentIndex < content.length - 1}
-                canClickPrevious={currentIndex !== 0}
-              />
-            </div>
-          </CSSTransition>
-        ))}
-      </div>
+      <div className={styles.slider_wrapper}>{children}</div>
     </section>
   );
 };
---
+++ src/components/Slide/SlideItem/SlideItem.tsx
@@ -0,0 +1,69 @@
+import React, { useRef } from "react";
+
+import { CSSTransition } from "react-transition-group";
+
+import { Player } from "../../audioPlayer/AudioPlayer";
+
+import { IData } from "../../../types";
+
+import styles from "./slide-item.module.scss";
+
+interface ISlideItem {
+  data: IData;
+  clickNextHandler: () => void;
+  clickPreviousHandler: () => void;
+  onEnded?: (() => void) | undefined;
+  length: number;
+  currentIndex: number;
+  index: number;
+  animation: {
+    enterActive: string;
+    enter: string;
+    exitActive: string;
+    exit: string;
+  };
+  timeout: number;
+}
+
+export const SlideItem: React.FC<ISlideItem> = ({
+  data,
+  clickNextHandler,
+  onEnded,
+  clickPreviousHandler,
+  length,
+  currentIndex,
+  index,
+  animation,
+  timeout,
+}) => {
+  const transitionRef = useRef(null);
+
+  return (
+    <CSSTransition
+      in={currentIndex === index}
+      nodeRef={transitionRef}
+      timeout={timeout}
+      unmountOnExit
+      classNames={animation}
+    >
+      <div
+        className={
+          index === currentIndex ? styles["is-visible"] : styles["v-hidden"]
+        }
+        ref={transitionRef}
+      >
+        <img src={data.imageURL} className={styles.cover} alt={data.text} />
+
+        <h3>{data.text}</h3>
+        <Player
+          src={data.audioURL}
+          clickNextHandler={() => clickNextHandler()}
+          onEnded={onEnded ? () => onEnded() : undefined}
+          clickPreviousHandler={() => clickPreviousHandler()}
+          canClickNext={currentIndex < length - 1}
+          canClickPrevious={currentIndex !== 0}
+        />
+      </div>
+    </CSSTransition>
+  );
+};
