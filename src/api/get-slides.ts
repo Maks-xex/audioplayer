4569b130cc46e67e0c019c2cd0dd124d16147b6d
Maks-xex <justmax.xex@mail.ru>
2023-11-04 10:18:42 +0100

    add try catch

--- src/api/get-slides.ts
+++ src/api/get-slides.ts
@@ -2,9 +2,15 @@ import { IData } from "../types";
 import { HOST } from "./constans";
 
 export const getSlides = async (): Promise<IData[]> => {
-  const response = await fetch(`${HOST}/api/slides`);
-  if (!response.ok) {
-    throw new Error(`${response.status}`);
+  try {
+    const response = await fetch(`${HOST}/api/slides`);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    return await response.json();
+  } catch {
+    throw new Error(
+      "An error occurred while fetching data. Please check your internet connection and try again."
+    );
   }
-  return await response.json();
 };
