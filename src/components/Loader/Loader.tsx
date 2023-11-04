0b0d9fc6f6c467bb9bda06e4da66c0100800a9de
Maks-xex <justmax.xex@mail.ru>
2023-11-04 10:16:44 +0100

    add test for Loader.tsx

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./loader.module.scss";
 
 export const Loader: React.FC = () => (
-  <div className={styles["center-both"]}>
+  <div className={styles["center-both"]} data-testid="loader">
     <div className={styles["lds-roller"]}>
       <div></div>
       <div></div>
