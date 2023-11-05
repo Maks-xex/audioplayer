f30954b07b3fa8d3abdc24877b7c09f25b3a80de
Maks-xex <justmax.xex@mail.ru>
2023-11-05 10:26:32 +0100

    update server

--- server/index.ts
+++ server/index.ts
@@ -2,23 +2,18 @@ import express, { Express, Request, Response } from "express";
 
 import path from "path";
 
-import dotenv from "dotenv";
-
 import cors from "cors";
 
 import slidesData from "./database/data.json";
 
-dotenv.config();
-
 const app: Express = express();
-const port = process.env.PORT;
+const port = 5000;
 
 app.use(cors());
 
 app.get("/api/slides", (_req: Request, res: Response) => {
   res.json(slidesData);
 });
-
 app.use(express.static(path.join(__dirname, "./database")));
 
 app.listen(port, () => {
